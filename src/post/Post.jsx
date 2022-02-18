dc5fa31232f12234d2021664868d26a044ef428f
Rolando Mata <rdma95@gmail.com>
2022-02-18 13:29:11 -0600

    blog posts grid working

---
+++ src/post/Post.jsx
@@ -0,0 +1,27 @@
+import "./post.css"
+
+export default function Post() {
+  return (
+    <div className='post'>
+        <img className="postImg" src="https://picsum.photos/id/237/200" alt="" />
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Photography</span>
+                <span className="postCat">Tech</span>
+                <span className="postCat">Films</span>
+                <span className="postCat">Music</span>
+            </div>
+            <span className="postTitle">Titulazo</span>
+            
+            <span className="postDate">1 hour ago</span>
+        </div>
+        <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Nisi vitae suscipit tellus mauris. Posuere lorem ipsum dolor sit amet. Risus sed vulputate odio ut enim. Urna nec tincidunt praesent semper. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing. At augue eget arcu dictum varius duis at consectetur. Urna condimentum mattis pellentesque id. Enim diam vulputate ut pharetra sit. Vivamus at augue eget arcu dictum varius duis at. Aliquam nulla facilisi cras fermentum. Amet consectetur adipiscing elit ut aliquam. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Nisl tincidunt eget nullam non nisi est sit amet. Facilisis sed odio morbi quis.
+
+        Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc. Proin nibh nisl condimentum id venenatis a condimentum vitae. Mi quis hendrerit dolor magna eget est lorem ipsum. Massa tincidunt dui ut ornare. Ut tellus elementum sagittis vitae et leo duis. Arcu non sodales neque sodales ut etiam sit. Nunc non blandit massa enim nec. Mus mauris vitae ultricies leo integer malesuada nunc vel. Tortor at risus viverra adipiscing at in tellus integer feugiat. Nec ultrices dui sapien eget mi. Eget dolor morbi non arcu risus quis varius quam quisque. Sit amet mattis vulputate enim nulla aliquet. Enim neque volutpat ac tincidunt vitae semper quis lectus nulla. Sit amet commodo nulla facilisi nullam vehicula ipsum a arcu. Amet mattis vulputate enim nulla aliquet porttitor. Faucibus in ornare quam viverra orci. Accumsan in nisl nisi scelerisque eu ultrices.
+
+        Nunc sed augue lacus viverra vitae congue eu consequat. Placerat duis ultricies lacus sed turpis tincidunt id aliquet. Sed turpis tincidunt id aliquet risus feugiat. Urna duis convallis convallis tellus id interdum. Aliquet lectus proin nibh nisl condimentum id. Iaculis urna id volutpat lacus laoreet non. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Nunc vel risus commodo viverra maecenas accumsan lacus. Sed ullamcorper morbi tincidunt ornare massa eget egestas. Urna condimentum mattis pellentesque id nibh tortor id aliquet lectus. Amet cursus sit amet dictum sit. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Volutpat sed cras ornare arcu dui vivamus arcu. Egestas sed tempus urna et. Nisl rhoncus mattis rhoncus urna neque viverra justo.  
+        </p>
+    </div>
+  )
+}
