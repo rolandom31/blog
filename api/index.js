8fd1b496d10917c32d54d56963f574514cf95915
Rolando Mata <rdma95@gmail.com>
2022-03-07 15:54:15 -0600

    login & register working
    backend login & register working

--- api/index.js
+++ api/index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
-dotenv.config()
+dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL,{
     
 }).then(console.log("connect to mongo")).catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+
 
 app.listen(('5003'),()=>{
     console.log('backend running');
