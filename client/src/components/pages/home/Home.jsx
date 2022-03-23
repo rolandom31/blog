15796fa340623e2c85bce4389917c8749c2d8072
Rolando Mata <rdma95@gmail.com>
2022-03-23 10:07:35 -0600

    bug proxy

--- client/src/components/pages/home/Home.jsx
+++ client/src/components/pages/home/Home.jsx
@@ -2,8 +2,20 @@ import './home.css'
 import Header from '../../header/Header'
 import Posts from '../../posts/Posts'
 import Sidebar from '../../sidebar/Sidebar'
+import { useEffect, useState } from "react";
+import axios, { Axios } from "axios"
+
 
 export default function Home() {
+  const [post,setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts")
+      console.log(res)
+    }
+    fetchPosts()
+  },[])
   return (
     <>
     <Header/>
