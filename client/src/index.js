7a417242c177bb9045317e65acc5d9fdf8e157e6
Rolando Mata <rdma95@gmail.com>
2022-03-25 13:37:20 -0600

    nodemon bug :(

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 import App from './App';
+import { ContextProvider } from "./context/Context";
 import "./palette.css"
 
 
 ReactDOM.render(
     <React.StrictMode>
+        <ContextProvider>
         <App/>
+        </ContextProvider>
+        
     </React.StrictMode>,
     document.getElementById('root')
 )
